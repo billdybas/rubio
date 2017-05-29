719101cc5f74b89e1f89cec4010f81181eadd80c
Bill Dybas <wmdybas@gmail.com>
2017-05-29 13:32:30 -0400

    Fix Export & Resolve Assumption

--- src/Foundation/index.js
+++ src/Foundation/index.js
@@ -5,6 +5,4 @@ import Provider from './Provider'
 import Service from './Service'
 import * as Utils from './Utils'
 
-// TODO: Test if this is how I think exporting Utils should work, or
-// if I need to reexport every function in Utils
-export default { Container, Provider, Service, Utils }
+export { Container, Provider, Service, Utils }
